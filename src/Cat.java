1fbaaa5e9a76a14b61314b02f11161827d8cc8b0
mishan-tharindu <v.mishantharindu@gmail.com>
2023-11-29 19:40:02 +0530

    Create Cat Class

---
+++ src/Cat.java
@@ -0,0 +1,25 @@
+public class Cat {
+
+    String name;
+    int age;
+    String color;
+    String breed;
+
+//    Intstance methods: behaviors of Cat
+
+    void sleep()
+    {
+        System.out.println("Sleeping");
+    }
+
+    void play()
+    {
+        System.out.println("Playing");
+    }
+
+    void feed()
+    {
+        System.out.println("Eating");
+    }
+
+}
