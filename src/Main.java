c746a67e08ea7c5bb19b4a072ad03f7ae7990d7b
mishan-tharindu <v.mishantharindu@gmail.com>
2023-11-29 21:24:08 +0530

    Define Cat Objects

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,26 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Cat thor = new Cat();
+        Cat rambo = new Cat();
+
+//        Define thor Cat
+        thor.name = "Thor";
+        thor.age = 3;
+        thor.breed = "Sri Lankan";
+        thor.color = "black";
+
+//        Define Rambo Cat
+        rambo.name = "Rambo";
+        rambo.age = 4;
+        rambo.breed = "Maine Coon";
+        rambo.color = "Brown";
+
+        thor.play();
+
+        rambo.sleep();
+
+
 
     }
 }
